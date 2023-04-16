abe5b7060abcd42b89a0637f2448407de3b6b37c
1148648777@qq.com <1148648777@qq.com>
2023-04-16 09:24:51 +0800

    feat(ip-address): Add feature to get local IP address and show.

---
+++ app/src/main/java/com/example/pandabox/utils/GetIP.kt
@@ -0,0 +1,22 @@
+package com.example.pandabox.utils
+
+import java.net.NetworkInterface
+import java.util.*
+
+fun getLocalIpAddress(): String? {
+    try {
+        val interfaces: List<NetworkInterface> =
+            Collections.list(NetworkInterface.getNetworkInterfaces())
+        for (intf in interfaces) {
+            val addresses = Collections.list(intf.inetAddresses)
+            for (address in addresses) {
+                if (!address.isLoopbackAddress && address.isSiteLocalAddress) {
+                    return address.hostAddress
+                }
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return null
+}
